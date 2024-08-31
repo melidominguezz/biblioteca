5ae880291ff39ddddbc816531419485c39b63854
Daniela Melian <meliandaani@gmail.com>
2024-08-31 19:05:18 -0300

    desarrollo de funcionalidades busqueda_libros, cargar_libros y obtener_libro

---
+++ books_data.py
@@ -0,0 +1,13 @@
+libros = [
+    # [Id interno del libro, Autor, Título, Género, ISBN, Editorial, Año de Publicación, Serie, Nro. Páginas, Cant. Ejemplares, Disponibilidad]
+    ["J.K. Rowling", "Harry Potter y la Piedra Filosofal", "Fantasía", 9780747532743, "Bloomsbury", 1997, "Harry Potter", 223, 5, True],
+    ["George R.R. Martin", "Juego de Tronos", "Fantasía", 9780553573404, "Bantam Books", 1996, "Canción de Hielo y Fuego", 694, 3, True],
+    ["Agatha Christie", "Diez Negritos", "Misterio", 9780007136834, "HarperCollins", 1939, "Hercule Poirot", 264, 7, False],
+    ["Gabriel García Márquez", "Cien Años de Soledad", "Realismo Mágico", 9780307474728, "Vintage", 1967, "N/A", 417, 4, False],
+    ["Dan Brown", "El Código Da Vinci", "Thriller", 9780307474278, "Doubleday", 2003, "Robert Langdon", 489, 6, True],
+    ["J.R.R. Tolkien", "La Comunidad del Anillo", "Fantasía", 9780618640157, "Houghton Mifflin", 1954, "El Señor de los Anillos", 423, 2, False],
+    ["Suzanne Collins", "Los Juegos del Hambre", "Distopía", 9780439023528, "Scholastic", 2008, "Los Juegos del Hambre", 374, 8, True],
+    ["Stephen King", "It", "Terror", 9780450411434, "Viking Press", 1986, "N/A", 1138, 5, False],
+    ["Isaac Asimov", "Fundación", "Ciencia Ficción", 9780553293357, "Spectra", 1951, "Fundación", 255, 4, True],
+    ["Haruki Murakami", "Norwegian Wood", "Ficción", 9780307476463, "Vintage", 1987, "N/A", 298, 3, False]
+]
