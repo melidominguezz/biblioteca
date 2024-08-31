035b1e59bc2069c3afcb4474f0b632f0c6cef55c
Daniela Melian <meliandaani@gmail.com>
2024-08-31 18:03:30 -0300

    arreglo de archivos con funciones, nueva disposicion: book_utils y user_utils

--- utils/book_utils.py
+++ utils/book_utils.py
@@ -25,3 +25,32 @@ def obtener_libro(id_libro, ISBN):
         :param ISBN: Int, International Standard Book Number del libro.
         :return libro: Dict, informacion detallada del libro buscado.
         """
+
+
+# Tenemos la lista con todos los libros disponibles
+# La lista con todos los libros alquilados
+# La función compara el libro que se consulta con todos los libros presentes en ambas listas. Si lo encuentra en alguna
+# Devuelve el estado pertinente, si no lo encuentra en ninguna de las dos es porque no está en la biblioteca
+
+def estatus_libros(n):
+    disponible = ["emma"]
+    en_espera = ["orgullo y prejuicio"]
+    final = ""
+
+    if n in disponible:
+        return "disponible"
+    elif n in en_espera:
+        return "en_espera"
+
+
+# Codigo principal
+
+libro = input("Ingrese el libro de su consulta")
+libro_buscado = estatus_libros(libro)
+
+if libro_buscado == "disponible":
+    print("lo tenemos")
+elif libro_buscado == "en_espera":
+    print("Ahora está reservado, pero vuelve")
+else:
+    print("No contamos con ese libro en nuestra biblioteca")
